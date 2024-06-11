ad3bbc55c982fdd481883ad664a6fbb61e5f6f21
rafsun-riad <mruhaquer@gmail.com>
2024-06-11 17:36:55 +0600

    working with jwt token

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from django.contrib.auth.models import User
 from base.models import Product
 
@@ -7,3 +9,14 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
--- base/api/views.py
+++ base/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from base.models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, MyTokenObtainPairSerializer
 
 
 # Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
