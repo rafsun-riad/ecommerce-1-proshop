799be8722302ddbdf1e7ad4a76777add3c586b8d
rafsun-riad <mruhaquer@gmail.com>
2024-06-18 15:21:00 +0600

    working with user login

--- frontend/src/features/users/usersSlice.js
+++ frontend/src/features/users/usersSlice.js
@@ -2,13 +2,21 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   userInfo: {},
+  isLoading: false,
+  isError: false,
+  error: null,
 };
 
 const userSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {},
+  reducers: {
+    userLogout: (state) => {
+      state.userInfo = {};
+    },
+  },
   extraReducers: {},
 });
 
+export const { userLogout } = userSlice.actions;
 export default userSlice.reducer;
