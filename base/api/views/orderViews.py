f1f1c6197e50dd900e86fa9fa85c8713292541e9
rafsun-riad <mruhaquer@gmail.com>
2024-06-30 15:42:27 +0600

    working with order view

---
+++ base/api/views/orderViews.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+
+from rest_framework import status
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No order items'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response('Order')
