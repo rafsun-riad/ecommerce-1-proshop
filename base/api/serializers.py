ed55b9c43b8f547e0c5725d25845caa9523c55cc
rafsun-riad <mruhaquer@gmail.com>
2024-07-03 15:13:14 +0600

    build order view

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -72,6 +72,28 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         mdoel = Order
         fields = '__all__'
+
+    def get_orders(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingAddress)
+        except:
+            address = False
+
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user)
+        return serializer.data
