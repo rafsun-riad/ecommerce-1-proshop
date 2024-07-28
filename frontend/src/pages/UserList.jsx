60c9ca20a215bbadb6f6e0fb68d69d1461cd4008
rafsun-riad <mruhaquer@gmail.com>
2024-07-28 22:25:17 +0600

    building user list page

---
+++ frontend/src/pages/UserList.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+function UserList() {
+  return <div></div>;
+}
+
+export default UserList;
