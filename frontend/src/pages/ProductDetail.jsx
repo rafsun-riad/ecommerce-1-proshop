cc73112681bf3e65676134ec08b454ac13be9fef
rafsun-riad <mruhaquer@gmail.com>
2024-08-16 16:01:34 +0600

    working with review

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -19,6 +19,8 @@ import { addToCart, removeFromCart } from '../features/cart/cartSlice';
 
 function ProductDetail({ history }) {
   const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
   const {
     productDetails: product,
@@ -26,6 +28,9 @@ function ProductDetail({ history }) {
     isError,
     error,
   } = useSelector((store) => store.products);
+
+  const { userInfo } = useSelector((state) => state.users);
+
   const dispatch = useDispatch();
 
   const { id } = useParams();
@@ -54,112 +59,133 @@ function ProductDetail({ history }) {
       ) : isError ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row>
-          <Col md={6}>
-            <Image src={product.image} alt={product.name} />
-          </Col>
+        <div>
+          <Row>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} />
+            </Col>
 
-          <Col md={3}>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <h3>{product.name}</h3>
-              </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                  color={'#f8e825'}
-                />
-              </ListGroup.Item>
-
-              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-              <ListGroup.Item>
-                Description: {product.description}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-
-          <Col md={3}>
-            <Card>
+            <Col md={3}>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <Row>
-                    <Col>Price:</Col>
-                    <Col>
-                      <strong>${product.price}</strong>
-                    </Col>
-                  </Row>
+                  <h3>{product.name}</h3>
                 </ListGroup.Item>
 
                 <ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                    </Col>
-                  </Row>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                    color={'#f8e825'}
+                  />
                 </ListGroup.Item>
 
-                {product.countInStock > 0 && (
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  Description: {product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
                   <ListGroup.Item>
                     <Row>
-                      <Col>Qty</Col>
-                      <Col xs="auto" className="my-1">
-                        <Form.Control
-                          as="select"
-                          value={qty}
-                          onChange={(e) => setQty(Number(e.target.value))}
-                        >
-                          {[...Array(product.countInStock).keys()].map((x) => (
-                            <option key={x + 1} value={x + 1}>
-                              {x + 1}
-                            </option>
-                          ))}
-                        </Form.Control>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
                       </Col>
                     </Row>
                   </ListGroup.Item>
-                )}
 
-                {item ? (
                   <ListGroup.Item>
-                    <Button
-                      style={{ width: '100%' }}
-                      onClick={handleRemoveFromCart}
-                      className="btn-block"
-                      type="button"
-                    >
-                      Remove from Cart
-                    </Button>
-                    <Link to="/cart">
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col xs="auto" className="my-1">
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  {item ? (
+                    <ListGroup.Item>
                       <Button
                         style={{ width: '100%' }}
+                        onClick={handleRemoveFromCart}
+                        className="btn-block"
                         type="button"
-                        variant="outline-dark"
-                        className="mt-1"
                       >
-                        Show Cart
+                        Remove from Cart
                       </Button>
-                    </Link>
+                      <Link to="/cart">
+                        <Button
+                          style={{ width: '100%' }}
+                          type="button"
+                          variant="outline-dark"
+                          className="mt-1"
+                        >
+                          Show Cart
+                        </Button>
+                      </Link>
+                    </ListGroup.Item>
+                  ) : (
+                    <ListGroup.Item>
+                      <Button
+                        style={{ width: '100%' }}
+                        onClick={handleAddToCart}
+                        className="btn-block"
+                        type="button"
+                        disabled={product.countInStock === 0}
+                      >
+                        Add to Cart
+                      </Button>
+                    </ListGroup.Item>
+                  )}
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={6}>
+              <h4>Reviews</h4>
+              {product.reviews.length === 0 && (
+                <Message variant="info"></Message>
+              )}
+              <ListGroup variant="flush">
+                {product.reviews.map((review) => (
+                  <ListGroup.Item key={review._id}>
+                    <strong>{review.name}</strong>
+                    <Rating value={review.rating} color="#f8e825" />
                   </ListGroup.Item>
-                ) : (
-                  <ListGroup.Item>
-                    <Button
-                      style={{ width: '100%' }}
-                      onClick={handleAddToCart}
-                      className="btn-block"
-                      type="button"
-                      disabled={product.countInStock === 0}
-                    >
-                      Add to Cart
-                    </Button>
-                  </ListGroup.Item>
-                )}
+                ))}
               </ListGroup>
-            </Card>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </div>
       )}
     </div>
   );
