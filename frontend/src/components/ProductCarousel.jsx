9e93c19fcc6d35168bd6afc60f525523ff18adcf
rafsun-riad <mruhaquer@gmail.com>
2024-08-24 15:40:27 +0600

    making carousel

---
+++ frontend/src/components/ProductCarousel.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Loader from './Loader';
+import Message from './Message';
+
+function ProductCarousel() {
+  return <div></div>;
+}
+
+export default ProductCarousel;
